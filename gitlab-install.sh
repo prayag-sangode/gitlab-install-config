55409d1bcee18460e023efac6d3eecf54cc1a60f
Prayag Sangode <67312119+prayag-sangode@users.noreply.github.com>
2022-05-23 00:03:38 +0530

    Update gitlab-install.sh

--- gitlab-install.sh
+++ gitlab-install.sh
@@ -14,5 +14,5 @@ sudo yum install postfix
 # Download & install gitlab repo
 curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ee/script.rpm.sh | sudo bash
 
-# Install gitlab
-sudo EXTERNAL_URL="https://gitlab.hostbread.com" yum install -y gitlab-ee
+# Install gitlab, replace url with your domain
+sudo EXTERNAL_URL="https://gitlab.example.com" yum install -y gitlab-ee
